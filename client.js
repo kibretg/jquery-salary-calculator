e7447994e741de3ff28647b73d4c88695d69ff7f
kibretg <kibretrg@gmail.com>
2019-05-17 16:23:28 -0500

    created handleClick function and table

--- client.js
+++ client.js
@@ -1,1 +1,27 @@
 console.log('I\'m loving it');
+
+$(document).ready(onReady);
+
+function onReady(){
+    console.log('in JQ');
+    $('.submit').on('click', handleClick);
+    
+}
+
+function handleClick() {
+    let firstName = $('.firstName').val();
+    let lastName = $('.lastName').val();
+    let idNum = $('.idNum').val();
+    let title = $('.title').val();
+    let annualSalary = $('.annualSalary').val();
+
+    $('.tableBody').append(`
+    <tr>
+        <td>${firstName}</td>
+        <td>${lastName}</td>
+        <td>${idNum}</td>
+        <td>${title}</td>
+        <td>${annualSalary}</td>
+        
+    </tr>`)
+}
