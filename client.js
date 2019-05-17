4e6a7fbab7428e66f59430ec75a74ed4b205d9ac
kibretg <kibretrg@gmail.com>
2019-05-17 17:38:14 -0500

    corrected syntax, added handleDeleteClick function

--- client.js
+++ client.js
@@ -4,24 +4,46 @@ $(document).ready(onReady);
 
 function onReady(){
     console.log('in JQ');
-    $('.submit').on('click', handleClick);
+    //event listener that will call handleClick function
+    $('#submit').on('click', handleClick); 
+    //event listener that will call handleDeleteClick function
+    $('#tableBody').on('click', '.deleteEmp', handleDeleteClick);
     
 }
 
 function handleClick() {
-    let firstName = $('.firstName').val();
-    let lastName = $('.lastName').val();
-    let idNum = $('.idNum').val();
-    let title = $('.title').val();
-    let annualSalary = $('.annualSalary').val();
+    console.log('button clicked');
+    
+    let firstName = $('#firstName').val();
+    let lastName = $('#lastName').val();
+    let idNum = $('#idNum').val();
+    let title = $('#title').val();
+    let annualSalary = $('#annualSalary').val();
 
-    $('.tableBody').append(`
+    $('#tableBody').append(`
     <tr>
         <td>${firstName}</td>
         <td>${lastName}</td>
         <td>${idNum}</td>
         <td>${title}</td>
         <td>${annualSalary}</td>
+        <td>
+            <button class="deleteEmp">Delete</button>
+        </td>
         
     </tr>`)
+
+    $('#firstName').val('');
+    $('#lastName').val('');
+    $('#idNum').val('');
+    $('#title').val('');
+    $('#annualSalary').val('');
+}
+
+function handleDeleteClick(){
+    console.log('Delete');
+
+    $(this).closest('tr').remove();
+
+    
 }
